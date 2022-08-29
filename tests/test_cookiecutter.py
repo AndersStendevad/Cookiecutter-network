0f3dd89058d8ee36f5e19e383d8a5e78d2987317
Anders Stendevad <andst@flowtale.ai>
2022-08-29 14:31:25 +0200

    added test

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -34,4 +34,14 @@ def test_lint(bake, helper):
     """check lint in baked project"""
     helper.check_result(run("tox -e lint", shell=True, capture_output=True))
 
-
+@pytest.mark.skip(reason="Too slow")
+def test_lab(bake, helper):
+    """check lab in baked project"""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if not s.connect_ex(('localhost', 8888)):
+            raise Exception("8888 port is in use!")
+    sleep(0.5)
+    p = Popen(["tox", "-e lab"])
+    sleep(45)
+    requests.get("http://0.0.0.0:8888")
+    p.terminate()
